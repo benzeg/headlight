fa695049573d0e9373ef1e21d7b3ae4284302ab6
Benze Gong <benzegongdeveloper@gmail.com>
2019-06-22 16:25:10 -0700

    condense to single file for now

--- specs.ts
+++ specs.ts
@@ -43,3 +43,14 @@ class Lighthouse implements Auditor {
     return await this.browser.kill();
   }
 }
+
+/*
+opts.port = chrome.port;
+return lighthouse(url, opts, config).then(results => {
+  // use results.lhr for the JS-consumeable output
+  // https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
+  // use results.report for the HTML/JSON/CSV output as a string
+  // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
+  return chrome.kill().then(() => results.report)
+});
+ */
