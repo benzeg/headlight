12165683ce1bdd8345b1835f9e1bc207f69abb8d
Benze Gong <benzegongdeveloper@gmail.com>
2019-07-16 12:59:45 -0700

    fix async timeout error

--- specs.ts
+++ specs.ts
@@ -1,87 +1,22 @@
-import * as chromeLauncher from 'chrome-launcher';
-import * as lighthouse from 'lighthouse';
+import Headlight from './index';
 import { expect } from 'chai';
 import 'mocha';
 
-interface Page {
-  url: string;
-}
-
-interface Auditor {
-  readonly timestamp: string;
-  result: Array<object>;
-  queue: Array<Page>;
-  addToQueue(p: Page): void;
-}
-
-class Headlight implements Auditor {
-  timestamp: string;
-  result: Array<object> = [];
-  queue: Array<Page> = [];
-  addToQueue(p: Page) {
-    this.queue.push(p); 
-  };
-  browser: {
-    options: object;
-    process: any;
-  } = {
-    options: {
-      port: 3040,
-      chromeFlags: ['--headless', '--disable-gpu']
-    },
-    process: null
-  };
-  lighthouseConfig: object = {
-    extends: 'lighthouse:default',
-    settings: {
-      output: ['json'],
-      onlyAudits: [],
-    }
-  };
-  public Ready: Promise<any>;
-  constructor() {
-    this.timestamp = new Date().toISOString();
-    this.Ready = new Promise((resolve, reject) => {
-      chromeLauncher.launch(this.browser.options).then(result => {
-        this.browser.process = result; 
-        resolve(this.browser.process);
-      }).catch(reject);
-    });
-  }
-  async disconnect(): Promise<any> {
-    return await this.browser.process.kill();
-  }
-  async run(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      
-    });
-  }
-  async audit(p: Page) {
-    const res = await lighthouse(p.url, this.browser.options, this.lighthouseConfig); 
-    return res.lhr;
-  }
-}
-
-/******
- * TEST
- ******/
-
-describe('Headlight', () => {
-  describe('Ready Function', () => {
+describe('Headlight', function() {
+  describe('Ready Function', function() {
     let worker = new Headlight();
-    it('has a browser process', () => {
-      return worker.Ready.then(()=>{
-        expect(worker.browser.process.port).to.equal(3040);
-      });
+    it('has a browser process', async function() {
+      await worker.Ready;
+      expect(worker.browser.process.port).to.equal(3040);
     });
   });
-  describe('audit Function', () => {
+  describe('audit Function', function() {
+    this.timeout(6000); // worker's audit process takes a long time
     let worker = new Headlight();
-    it('outputs an object', () => {
+    it('outputs an object', async function() {
       const page = { url: 'https://www.madison-reed.com' }; 
-      worker.audit(page).then((output) => {
-        expect(output).to.be.an('object');
-      });
+      const output = await worker.audit(page);
+      expect(output).to.be.a('object');
     });
   });
 });
