f2c49f1ca82f1299cb0fdcc18ffbab0e934feddc
Benze Gong <benzegongdeveloper@gmail.com>
2019-08-26 17:54:48 -0700

    pass filename to historian and remove call to getReport

--- index.ts
+++ index.ts
@@ -1,8 +1,5 @@
 import { Historian } from './src/Historian';
 
-const historian = new Historian();
+const historian = new Historian({ filename: 'test.json' });
 
 historian.addToQueue({ url: 'https://benzeg.github.io'});
-historian.getReport().then((res)=> {
-  console.log('result', res);
-}).catch((e)=> console.error('error', e));
